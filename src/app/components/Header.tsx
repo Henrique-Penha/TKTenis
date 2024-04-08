0f5fd78e548bb2863ce287a229aabb9702a1a5f2
Henrique Penha <henrique-dsp34@hotmail.com>
2024-04-08 16:23:51 -0300

    Starting Project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import styled from "styled-components";
+
+const Header = () => {
+    return (
+        <>
+            <StyledHeader>TKTenis</StyledHeader>
+        </>
+    )
+}
+
+const StyledHeader = styled.header`
+    background-color: black;
+    color: white;
+`;
+
+export default Header;
