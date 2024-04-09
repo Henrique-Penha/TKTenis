221dd2e16e1e381cd4a3fd5520b4efd5c688e078
Henrique Penha <henrique-dsp34@hotmail.com>
2024-04-09 15:45:02 -0300

    (feat): Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,80 @@
 'use client';
 
+import Image from "next/image";
+import Link from "next/link";
 import styled from "styled-components";
+
+import Car from "../../../public/carrinho.png";
+import Logo from "../../../public/TK-logo.jpg";
 
 const Header = () => {
     return (
         <>
-            <StyledHeader>TKTenis</StyledHeader>
+            <StyledHeader>
+                <StyledNavbar>
+                    <StyledLogo>
+                        <Link href="/">
+                            <Image src={Logo} width={100} height={100} alt="Carrinho" />
+                        </Link>
+                    </StyledLogo>
+                    <StyledMenu>
+                        <StyledItems>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </StyledItems>
+                        <StyledItems>
+                            <Link href="">
+                                Sobre
+                            </Link>
+                        </StyledItems>
+                        <StyledItems>
+                            <Link href="">
+                                Produtos
+                            </Link>
+                        </StyledItems>
+                        <StyledItems>
+                            <Link href="">
+                                <Image src={Car} width={50} height={50} alt="Carrinho" />
+                            </Link>
+                        </StyledItems>
+                    </StyledMenu>
+                </StyledNavbar>
+            </StyledHeader>
         </>
     )
 }
 
 const StyledHeader = styled.header`
+    width: 100vw;
+    padding: 30px 60px;
     background-color: black;
     color: white;
+    border-bottom: solid blue;
+`;
+
+const StyledNavbar = styled.nav`
+    display: grid;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+`;
+
+const StyledLogo = styled.p`
+
+`;
+
+const StyledMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    list-style: none;
+    gap: 50px;
+`;
+
+const StyledItems = styled.li`
+    :hover {
+        color: pink;
+    }
 `;
 
 export default Header;
