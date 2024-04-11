cf028b343524bdb2bf53fa652bcb2902f9e14e02
Henrique Penha <henrique-dsp34@hotmail.com>
2024-04-11 12:21:01 -0300

    (feat): add info

---
+++ src/app/components/Info.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+import { Container } from "@/styles/util";
+
+import Image from "next/image";
+
+import Twhite from "../../../public/tenis-tree.jpg";
+import Tgold from "../../../public/tenis-five.jpg";
+
+const Info = () => {
+    return(
+        <StyledSec>
+            <StyledColum>
+                <StyledImg src={Twhite} width={390} height={350} alt="Tenis-white" />
+                <StyledText>Preços imbativeis com até 12x sem juros no cartão de credito.</StyledText>
+            </StyledColum>
+            <StyledColum>
+                <StyledText>Todos os estilos pra você provar e entrar no mundo da moda com elegância que só você tem.</StyledText>
+                <StyledImg src={Tgold} width={390} height={350} alt="Tenis-gold" />
+            </StyledColum>
+        </StyledSec>
+    )
+}
+
+const StyledSec = styled.section`
+    ${Container};
+    background-color: #CCC;
+`;
+
+const StyledColum = styled.div`
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    grid-template-columns: 1fr 1fr;
+    padding: 40px;
+`;
+
+const StyledImg = styled(Image)`
+    border-radius: 10%;
+`;
+
+const StyledText = styled.p`
+    text-align: justify;
+    font-size: 25px;
+    max-width: 20ch;
+    letter-spacing: 1px;
+    line-height: 50px;
+`;
+
+export default Info;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from "./components/Header"
 import Hero from "./components/Hero";
+import Info from "./components/Info";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Info />
     </main>
   );
 }
